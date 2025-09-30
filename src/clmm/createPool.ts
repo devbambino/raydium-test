ab388f6afc900838da843f90a2998745ca0b8778
Dev Bambino <devbambinoacc@gmail.com>
2025-09-30 16:39:40 +0000

    other

--- src/clmm/createPool.ts
+++ src/clmm/createPool.ts
@@ -17,18 +17,13 @@ export const createPool = async () => {
   // const clmmConfigs = devConfigs // devnet configs
 
   const { execute } = await raydium.clmm.createPool({
-    programId: CLMM_PROGRAM_ID,
-    // programId: DEVNET_PROGRAM_ID.CLMM_PROGRAM_ID,
+    programId: DEVNET_PROGRAM_ID.CLMM_PROGRAM_ID,
     mint1,
     mint2,
     ammConfig: { ...clmmConfigs[0], id: new PublicKey(clmmConfigs[0].id), fundOwner: '', description: '' },
     initialPrice: new Decimal(1),
     txVersion,
-    // optional: set up priority fee here
-    // computeBudgetConfig: {
-    //   units: 600000,
-    //   microLamports: 46591500,
-    // },
+    startTime: new BN
   })
   // don't want to wait confirm, set sendAndConfirm to false or don't pass any params to execute
   const { txId } = await execute({ sendAndConfirm: true })
